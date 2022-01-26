4d8acc61a350505eaa555399944db8e5bbde48f2
max-che <z3r0krank@gmail.com>
2022-01-26 03:33:26 +0300

    trying to send block of data

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "main.h"
 #include "uart_api.h"
 #include "stm32f10x_it.h"
+#include <stdlib.h>
 
 
 
@@ -13,6 +14,30 @@ void uart2_send_byte(char b) {
     uart_send_byte(USART2, b);
 }
 
+
+typedef struct block_s {
+    struct block_s *next;
+    char data[128];
+}block_t;
+
+void add_block(block_t * block) {
+    //block->next = malloc(sizeof(block_t));
+    //char* p = malloc(sizeof(char)*10);
+    return;
+}
+
+void del_block() {
+    
+}
+
+
+char *p_rx;
+char *p_tx;
+
+void rx_handler(char b) {
+        *p_rx = b;
+        p_rx++;
+    }
 
 
 int main(void) {    
@@ -27,11 +52,27 @@ int main(void) {
     set_uart_baud_rate(USART1, 9600);
     set_uart_baud_rate(USART2, 115200);
     
-    uart_set_callback_rx(USART1, uart2_send_byte);
-    uart_set_callback_rx(USART2, uart1_send_byte);
+    //uart_set_callback_rx(USART1, uart2_send_byte);
+    //uart_set_callback_rx(USART2, uart1_send_byte);
+    uart_set_callback_rx(USART1, rx_handler);
+    
+    
+    char block[128];
+    
+    p_rx = block;
+    p_tx = block;
+    
+    
+    
     
     
     while(1) {
+        
+        if(p_rx > p_tx) {
+            p_tx++;
+            uart_send_byte(USART2, *p_tx);
+        }
+        
         //GPIOC->ODR = 0x00000100;
         //ms_delay(100);
         //GPIOC->ODR = 0x00000200;
